83138d81b9cdb983c49dd841db41fe5403040085
tomasz-paluch <tomasz.m.paluch@gmail.com>
2020-10-09 12:41:55 +0200

    Added MongoDB & Mongoose connection

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+// import mongoose from 'mongoose';
+
 const app = express();
 const PORT = 4000;
 
@@ -15,3 +17,9 @@ app.get('/', (req, res) =>
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`)
 );
+
+const mongoose = require('mongoose');
+// https://stackoverflow.com/questions/51862570/mongoose-why-we-make-mongoose-promise-global-promise-when-setting-a-mongoo
+mongoose.Promise = global.Promise;
+// https://mongoosejs.com/docs/index.html
+mongoose.connect('mongodb://localhost/productsdb', {useNewUrlParser: true, useUnifiedTopology: true});
